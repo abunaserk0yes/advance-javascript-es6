ea95af1c4c8367035fcba04d55fc097f3875b33f
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-02 23:12:33 +0600

    end

---
+++ splice-vs-spice-vs-join.js
@@ -0,0 +1,19 @@
+numbers=[10,20,34,35,98,87,54,76,90,65,54];
+
+// Use splice 
+
+const splice=numbers.splice(3,6)
+console.log(splice);
+console.log(numbers);
+
+// Use slice 
+
+const slice=numbers.slice(3,6)
+console.log(slice);
+console.log(numbers);
+
+// Use join
+
+const join=numbers.join(',');
+console.log(join);
+
