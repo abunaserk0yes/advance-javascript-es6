b02944cf3a0f0c4a65ae13e9e696d8fcad8048df
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-02 10:27:32 +0600

    Setup now

---
+++ true-false.js
@@ -0,0 +1,21 @@
+/* falsy-value*/
+
+// const num=0
+// const num=undefined;
+// const num=false
+// const num=null;
+// const num='';
+
+/*truthy-value*/
+
+// const num=' ';
+// const num={};
+// const num=[];
+// const num='0';
+// const num='false';
+
+if (num) {
+    console.log('Condition is true');
+} else {
+    console.log('Condition is false');
+}
