034bea6118529e859f51f4ccee71ba3ac3aeb3ee
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-02 16:34:39 +0600

    finished

---
+++ double-vs-single.js
@@ -0,0 +1,11 @@
+if (2==='2') {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
+
+if (0==false) {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
