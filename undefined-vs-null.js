492842e1619acfce1b66732a8223dd49162a1593
Md Abu Naser Kayes <abunaserkayes3305@yahoo.com>
2021-02-02 16:16:03 +0600

    finish work

---
+++ undefined-vs-null.js
@@ -0,0 +1,19 @@
+let number;
+
+function getInput(num1,num2){
+    console.log(num1+num2);
+    return;
+}
+
+function getInput(num1,num2){
+    return;
+}
+
+const k0yes=[Name="carnival",age=10,service='sub-officer'];
+console.log(k0yes.bf);
+
+const funny=undefined;
+console.log(funny);
+
+const array=[2,5,6,7,6];
+console.log(array[10]);
